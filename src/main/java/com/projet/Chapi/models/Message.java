1c55bbde9386052506c1ebc00d5841127408c6f9
DenisNDJ <denis.ndjanda@groupe-esigelec.org>
2025-12-30 13:40:27 +0100

    suppression des getter et setter. Existe via @data

--- src/main/java/com/projet/Chapi/models/Message.java
+++ src/main/java/com/projet/Chapi/models/Message.java
@@ -23,44 +23,4 @@ public class Message {
     private String message;
     
     private String created_at;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public Long getRental_id() {
-		return rental_id;
-	}
-
-	public void setRental_id(Long rental_id) {
-		this.rental_id = rental_id;
-	}
-
-	public Long getUser_id() {
-		return user_id;
-	}
-
-	public void setUser_id(Long user_id) {
-		this.user_id = user_id;
-	}
-
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
-	}
-
-	public String getCreated_at() {
-		return created_at;
-	}
-
-	public void setCreated_at(String created_at) {
-		this.created_at = created_at;
-	}
 }
